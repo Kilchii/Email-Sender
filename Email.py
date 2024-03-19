781b42d15c89e65c797c108bb2d7dae5f0f7c5a7
Kilchii <57159126+Kilchii@users.noreply.github.com>
2024-03-19 00:04:29 +0000

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,27 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = "dreamline32.africa@gmail.com"
+email_password = "oxjz splt gxpv qocj"
+email_receiver = "fynnwilliamsbetty@yahoo.com"
+
+subject = "Guess What"
+
+body = """
+    This is a test email.
+"""
+em = EmailMessage()
+
+em ['From'] = email_sender
+em ['To'] = email_receiver
+em ['Subject'] = subject
+
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
